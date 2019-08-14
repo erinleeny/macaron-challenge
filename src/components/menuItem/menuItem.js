b101ce42c784ca62570f7ed299cb2baa0d4d12da
Erin Lee <erin.lee.ny@gmail.com>
2019-08-14 13:57:29 -0400

    edits

--- src/components/menuItem/menuItem.js
+++ src/components/menuItem/menuItem.js
@@ -6,24 +6,25 @@ import Grid from '@material-ui/core/Grid';
 
 class Menuitem extends Component {
     render() {
+        const {item, onAdd} = this.props;
         const buttonStyle = {color: "white", backgroundColor: "rgba(35, 121, 88, 0.75)", boxShadow: 'none', fontFamily: "Poppins", textTransform: 'none', height: "2.7rem", fontSize: 12, width: "80%", borderRadius: "5px"};
         const clickedStyle = {color: "white", backgroundColor: "#2EAE82", boxShadow: 'none', fontFamily: "Poppins", textTransform: 'none', height: "2.7rem", fontSize: 12, width: "80%", borderRadius: "5px"}
         return (
             <React.Fragment>
             <Grid container className = "menuItem">
                 {this.props.children}
-                <Grid xs = {2}><img className = "img" src = {this.props.item.imgUrl}/></Grid>
+                <Grid xs = {2}><img className = "img" src = {item.imgUrl}/></Grid>
                 <Grid xs = {6}><div className = "box">
-                    <h2 className = "h2" style = {{fontWeight: 500, color: "rgba(35, 121, 88, 0.75)", marginTop: "-1rem"}}>{this.props.item.name}</h2>
-                    <h3 className = "amount" >{this.props.item.amount}</h3>
-                    <h3 style = {{fontWeight: 200}}>{this.props.item.text}</h3>
+                    <h2 className = "h2" style = {{fontWeight: 500, color: "rgba(35, 121, 88, 0.75)", marginTop: "-1rem"}}>{item.name}</h2>
+                    <h3 className = "amount" >{item.amount}</h3>
+                    <h3 style = {{fontWeight: 200}}>{item.text}</h3>
                 </div></Grid><br/>
                 <Grid xs = {1}/>
                 <Grid xs = {3}><div className = "price">
-                    <br/><h3 className = "priceName">${this.props.item.price}/ea.</h3>
-                    {this.props.item.added
+                    <br/><h3 className = "priceName">${item.price}/ea.</h3>
+                    {item.added
                         ? <Button style = {clickedStyle}><span className = "buttonText"><DoneIcon style = {{fontSize: 13, marginBottom: -3, paddingRight: "0.3rem"}} />Added to Cart</span></Button>
-                        : <Button onClick = {() => this.props.onAdd(this.props.item)} variant="contained" style = {buttonStyle} ><span className = "buttonText">+ Add to Cart</span></Button>
+                        : <Button onClick = {() => onAdd(item)} variant="contained" style = {buttonStyle} ><span className = "buttonText">+ Add to Cart</span></Button>
                     }
                 </div></Grid>
             </Grid>
